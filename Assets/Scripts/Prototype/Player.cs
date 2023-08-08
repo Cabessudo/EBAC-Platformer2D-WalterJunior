6d49408f0e2db32c5356edb8f99e039ddca2e582
Cabessudo <waltersdjunior@gmail.com>
2023-08-08 12:03:40 -0400

    added jump and friction to prototype

--- Assets/Scripts/Prototype/Player.cs
+++ Assets/Scripts/Prototype/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rb;
     private Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +20,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump();
         Movement();
     }
 
@@ -30,6 +33,23 @@ public class Player : MonoBehaviour
         else if(Input.GetKey(KeyCode.A))
         {
             _rb.velocity = new Vector2(-speed, _rb.velocity.y);
+        }
+
+        if(_rb.velocity.x > 0)
+        {
+            _rb.velocity -= friction;            
+        }
+        else if(_rb.velocity.x < 0)
+        {
+            _rb.velocity += friction;
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.velocity = Vector3.up * jumpForce;
         }
     }
 }
