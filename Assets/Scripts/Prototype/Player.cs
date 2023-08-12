1504c8c9b928af5ee4fa34f5d98258045c745ff6
Cabessudo <waltersdjunior@gmail.com>
2023-08-12 13:02:16 -0400

    added jump animation

--- Assets/Scripts/Prototype/Player.cs
+++ Assets/Scripts/Prototype/Player.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening; 
 
 public class Player : MonoBehaviour
 {
 
+    [Header("References")]
     private Rigidbody2D _rb;
+
+    [Header("Movement")]
     private Vector2 velocity;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float speedRun;
     public float jumpForce;
+
+    [Header("Animation")]
+    public float duration;
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.5f;
+    public Ease ease = Ease.OutBack;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +61,17 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             _rb.velocity = Vector3.up * jumpForce;
+            _rb.transform.localScale = Vector2.one;
+
+            DOTween.Kill(_rb.transform);
+            
+            JumpAnimation();
         }
+    }
+
+    void JumpAnimation()
+    {
+        _rb.transform.DOScaleY(jumpScaleY, duration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        _rb.transform.DOScaleX(jumpScaleX, duration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
