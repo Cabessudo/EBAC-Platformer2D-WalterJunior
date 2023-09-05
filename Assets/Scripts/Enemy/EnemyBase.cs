fa1e4be1c2890ba2fd009efc11f619c8d4cf4b71
Cabessudo <waltersdjunior@gmail.com>
2023-09-04 22:33:04 -0400

    added movement to the enemy

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -6,8 +6,17 @@ using DG.Tweening;
 public class EnemyBase : MonoBehaviour
 {
 
+    [Header("References")]
+    public Rigidbody2D rb;
+    public Ease ease;
+
     [Header("Damage")]
     public int damage = 1;
+
+    void Start()
+    {
+        rb.transform.DOMoveX(-3, 1).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
