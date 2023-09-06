1e19442ccf9815ec8b49438925f3c3e4d742eff7
Cabessudo <waltersdjunior@gmail.com>
2023-09-05 21:11:46 -0400

    added a satelite and an animation to it

---
+++ Assets/Scripts/AnimationScript/AnimationTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+
+    public Animator anim;
+    public KeyCode keyToFly = KeyCode.A;
+    public string  triggerToPlay = "Fly";
+
+    private void OnValidate()
+    {
+        if(anim == null) anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToFly))
+        anim.SetBool(triggerToPlay, !anim.GetBool(triggerToPlay));
+    }
+}
