61cb261855dc301415aa749d2824b2bab411c62b
Cabessudo <waltersdjunior@gmail.com>
2023-09-23 00:38:28 -0400

    updated camera view to perspective, and made some changes

--- Assets/Scripts/Cameras/FollowPlayer.cs
+++ Assets/Scripts/Cameras/FollowPlayer.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class FollowPlayer : MonoBehaviour
 {
     private GameObject _player;
+    public Ease ease;
+    private bool _start;
+    public float timeToStart = 1;
+    public float duration = 1;
+    public float playerDis = 3;
+    public float yPos = -2.5f;
+
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
+        
+        StartCoroutine(AnimStart());
     }
     // Update is called once per frame
     void Update()
     {
-        if(_player != null)
-        transform.position = _player.transform.position;       
+        if(_player != null && _start)
+        StartFollowPlayer();
+    }
+
+    IEnumerator AnimStart()
+    {
+        transform.DOMoveY(yPos, duration).SetEase(ease);
+        yield return new WaitForSeconds(timeToStart);
+        _start = true;
+    }
+
+    void StartFollowPlayer()
+    {
+        transform.position = new Vector3(_player.transform.position.x, transform.position.y, transform.position.z);     
     }
 }
